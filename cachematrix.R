7c75b9fa8029a12be1d52681879a7c49c6a7ffc4
JS Mac <63420516+anorexicslug@users.noreply.github.com>
2020-05-04 14:58:59 -0400

    Final Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program will attempt to return the inverse of a square
+## matrix by checking to see if the inverse has already been 
+## calculated and returning the value
 
-## Write a short comment describing this function
+## the makeCacheMatrix function creates the object makeCascheMatrix
+## that gets and sets the value of the inverse of the matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+  inv <- NULL
+  ##set function
   set <- function(y) {
     x <<- y
-    m <<- NULL
+   inv <<- NULL
   }
+  ## get function
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  ## defines a setter for the solve function
+  setinv <- function(solve) inv <<- solve
+  ## defines a getter for the solve function
+  getinv <- function() inv
+  
+  ##the list returned allow the $ to access function by name
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setinv = setinv,
+       getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is used to populate or retieve the inverse of the 
+## makeCacheVector object
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  m <- x$getmean()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
+  inv <- x$getinv()
+  ## check to see if the inverse exists then retrieves it
+  if(!is.null(inv)) {
+    message("getting cached inverse matrix data")
+    return(inv)
   }
+  ## If it does not exist, data is assign the value from the get
   data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
+  ## inverse is calculated
+  inv <- solve(data, ...)
+  ## the value is now set
+  x$setinv(inv)
+  ## the inverse returned
+  inv
 }
